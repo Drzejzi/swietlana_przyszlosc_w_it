243f34d35e6e159b9060f24af598810537226395
Drzejzi <drzejzi@gmail.com>
2016-11-12 12:00:31 +0100

    Added calculations

--- c++/helloworld.cpp
+++ c++/helloworld.cpp
@@ -4,7 +4,13 @@ using namespace std;
  
 int main()
 {
-    cout<<"Hello World!";
+	int liczba_a = 2;
+	int liczba_b = 3;
+	int liczba_c = liczba_a * liczba_b;
+	
+    cout << "Hello World! The result is: ";
+    cout <<  liczba_c;
+    
     cin.get();
     return 0;
 }
